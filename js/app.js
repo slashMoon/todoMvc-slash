99b790f13e72253fca4063c0d47d019ebae533bd
xiao <xiao@xiao.com>
2017-06-27 20:48:36 +0800

    03-完整功能实现

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@
 
 	angular
 		.module('todoApp', [])
-		.controller('TodoController', ['$scope', TodoController]);
+		.controller('TodoController', ['$scope', '$location', TodoController]);
 
 
 	//控制器函数
-	function TodoController($scope) {
+	function TodoController($scope, $location) {
 		var vm = $scope;
 
 		//
@@ -70,12 +70,12 @@
 			todoList.forEach(function (todo) {
 				todo.isCompleted = vm.isCheckAll;
 			});
-			
+
 		};
 
 		//清除已完成任务
 		vm.clearCompleted = function () {
-			// 将已经完成的
+			// 将未完成的加到新的数组当中
 			var tempArr = [];
 			for (var i = 0; i < todoList.length; i++) {
 				var todo = todoList[i];
@@ -88,6 +88,29 @@
 			[].push.apply(vm.todoList, tempArr);
 		};
 
+		//显示隐藏清除完成按钮
+		vm.isShow = function () {
+
+			return todoList.some(function (todo, index) {
+
+				if (todo.isCompleted) {
+					return true
+				}
+			})
+		}
+
+		// 显示未完成的任务
+		vm.getItem = function(){
+			var count = 0;
+			todoList.forEach(function(todo){
+				if(!todo.isCompleted){
+					count++;
+				}
+
+			})
+			return count;
+		}
+
 		//显示不同状态的任务
 		// 以及当前任务高亮处理
 		//通过angular中的过滤器
@@ -95,21 +118,41 @@
 		//展示未完成的任务： false
 		// 展示所有的设置为udefined
 		vm.status = undefined;
-		vm.showAll = function(){
-			vm.status = undefined;
-		}
-		vm.showActive = function(){
-			vm.status = false;
-		};
-		vm.showCompleted = function(){
-			vm.status = true;
-		}
+		// vm.showAll = function () {
+		// 	vm.status = undefined;
+		// }
+		// vm.showActive = function () {
+		// 	vm.status = false;
+		// };
+		// vm.showCompleted = function () {
+		// 	vm.status = true;
+		// }
 
 		//9 n根据url变化显示相应的任务
-		// 思路：监视url中哦你hash值的变化
+		// 思路：监视url中hash值的变化
 		// $scope.$watch()
 
-		//通过 $
+		//通过 $location.url()来获取地址栏
+		// 
+
+		vm.location = $location
+		console.log($location.url())
+
+
+		vm.$watch("location.url()", function (curvalue) {
+			switch (curvalue) {
+				case "/#%2Fcompleted":
+					vm.status = true;
+					break
+				case "/#%2Factive":
+					vm.status = false;
+					break;
+					default:
+					vm.status = undefined;
+					break;
+			}
+
+		})
 
 
 
